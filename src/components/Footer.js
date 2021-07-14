9d8968f7c2c4a741b3a62ce231535ede20ddb010
Cat Ormerod <catormerod@gmail.com>
2021-07-15 09:52:13 +1000

    added basic footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    
+    return (
+        <footer>
+            <a href="https://github.com/catormerod" target="_blank">
+            GitHub</a>
+            <a href="https://www.linkedin.com/in/cat-ormerod/" target="_blank">
+            LinkedIn</a>
+        </footer>
+    );
+};
+
+export default Footer;
