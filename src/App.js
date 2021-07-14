eccb5ec4a3a50cb10e14e405f82f200e23a151dc
Cat Ormerod <catormerod@gmail.com>
2021-07-15 09:51:53 +1000

    Removed Portfolio container as redundant

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, { useState, useEffect } from 'react';
+import About from './components/pages/About.js';
+import Contact from './components/pages/Contact.js';
+import Footer from './components/Footer.js';
+import Nav from './components/Nav.js';
+import Portfolio from './components/pages/Portfolio.js';
+import Resume from './components/pages/Resume.js';
 
-const App = () => <PortfolioContainer />;
+function App() {
+
+    const [pages] = useState([
+        { name : 'About' },
+        { name: 'Portfolio' },
+        { name: 'Resume' },
+        { name: 'Contact' }
+    ]);
+    const [currentPage, setCurrentPage] = useState(pages[0]);
+
+    useEffect(() => {
+        document.title = `CatOrmerod :: ${currentPage.name}`
+    });
+
+    return (
+        <div>
+            <Nav 
+                pages={pages}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            />
+            <main>
+                {currentPage.name === 'About' && <About />}
+                {currentPage.name === 'Portfolio' && <Portfolio />}
+                {currentPage.name === 'Resume' && <Resume />}
+                {currentPage.name === 'Contact' && <Contact />}
+            </main>
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
