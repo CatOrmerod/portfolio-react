d2c81e9a2383b89d3df1f142de1106f71f73fa57
Cat Ormerod <catormerod@gmail.com>
2021-07-15 09:52:42 +1000

    Nav working with reactstrap

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,52 +1,36 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-import classnames from 'classnames';
 
 const Navigation = (props) => {
-    const [collapsed, setCollapsed] = useState(false);
-    const toggleNavbar = () => setCollapsed(!collapsed);
-    const [activeTab, setActiveTab] = useState('1');
-    const toggle = tab => {
-    if(activeTab !== tab) setActiveTab(tab);
-  }
-  
-    return (
-      <div>
-        <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto">Coding Cat Portfolio</NavbarBrand>
-          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!collapsed} navbar>
-            <Nav navbar>
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+  const {
+    pages = [],
+    setCurrentPage,
+    currentPage
+  } = props;
+
+  return (
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">Coding Cat Portfolio</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            {pages.map(page => (
               <NavItem>
-                <NavLink 
-                href="/About/" 
-                className={classnames({ active: activeTab === '1' })}
-                onClick={() => { toggle('1'); }}
-                >About</NavLink>
+                <NavLink
+                  className={`${page.name === currentPage.name}`}
+                  key={page.name}
+                  onClick={() => setCurrentPage(page)}
+                >{page.name}</NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink href="/Portfolio/"
-                className={classnames({ active: activeTab === '2' })}
-                onClick={() => { toggle('2'); }}
-                >Portfolio</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/Resume/"
-                className={classnames({ active: activeTab === '3' })}
-                onClick={() => { toggle('3'); }}
-                >Resume</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/Contact/"
-                className={classnames({ active: activeTab === '4' })}
-                onClick={() => { toggle('4'); }}
-                >Contact</NavLink>
-              </NavItem>
+              ))}
             </Nav>
           </Collapse>
         </Navbar>
-      </div>
+      </div >
     );
   }
-  
-  export default Navigation;
+
+export default Navigation;
