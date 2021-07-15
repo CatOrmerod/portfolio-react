990883727deda1f4e5aaec074f7d116a81b225e3
Cat Ormerod <catormerod@gmail.com>
2021-07-15 11:08:00 +1000

    adjusted layout of the cards

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,6 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import Modal from '../Modal.js';
-import { Card, Button, CardTitle, CardText, CardFooter, CardBody, CardImg, Row, Col } from 'reactstrap';
+import { Card, Button, CardTitle, CardText, CardFooter, CardBody, CardImg, Row, Col, Container } from 'reactstrap';
 import { projects } from '../../lib/projects.js'
 
 const Portfolio = (props) => {
@@ -11,26 +11,28 @@ const Portfolio = (props) => {
         setIsModalOpen(!isModalOpen);
     };
     return (
-        <Row>
+        <Container>
             {isModalOpen && <Modal selectedProject={selectedProject} onClose={toggleModal} />}
-            <Col sm="6">
+            <Row xs="1" sm="2" md="3">
                 {projects.map(project => (
-                    <Card>
-                        <CardImg top width="100%" src={project.projectPic} alt="" />
-                        <CardBody>
-                            <CardTitle tag="h5">{project.projectTitle}</CardTitle>
-                            <CardText className="text-muted">{project.projectRole}</CardText>
-                        </CardBody>
-                        <CardFooter>
-                            <Button 
-                                key={project.id}
-                                onClick={() => toggleModal(project)}
-                            >See More</Button>
-                        </CardFooter>
-                    </Card>
+                    <Col>
+                        <Card>
+                            <CardImg top width="100%" src={project.projectPic} alt="" />
+                            <CardBody>
+                                <CardTitle tag="h5">{project.projectTitle}</CardTitle>
+                                <CardText className="text-muted">{project.projectRole}</CardText>
+                            </CardBody>
+                            <CardFooter>
+                                <Button
+                                    key={project.id}
+                                    onClick={() => toggleModal(project)}
+                                >See More</Button>
+                            </CardFooter>
+                        </Card>
+                    </Col>
                 ))}
-            </Col>
-        </Row>
+            </Row>
+        </Container>
     );
 };
 
