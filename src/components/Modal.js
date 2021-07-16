7a76fe1ec1366d2427268dbbf2795ae23ccddb37
Cat Ormerod <catormerod@gmail.com>
2021-07-16 18:11:40 +1000

    updated modal object and included screenshot

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, List, ListInlineItem } from 'reactstrap';
 
 const ModalExample = ({ isOpen, onClose, selectedProject }) => {
+    console.log(selectedProject);
     const {
         projectTitle,
         projectTools,
         projectURL,
         projectRepo,
         projectRole,
-        projectDescription
+        projectDescription,
+        projectScreenshot
     } = selectedProject;
 
     return (
         <Modal isOpen={isOpen} toggle={onClose}>
             <ModalHeader toggle={onClose}>{projectTitle}</ModalHeader>
             <ModalBody>
+            <img src={projectScreenshot} alt="" style={{ width:'90%', maxWidth:'300px'}} />
                 <List type="inline">
                     <ListInlineItem>Description: {projectDescription}</ListInlineItem>
                     <ListInlineItem>Tools: {projectTools}</ListInlineItem>
