640537afaf97196dc1aca7abc175fc21f05ff44a
Cat Ormerod <catormerod@gmail.com>
2021-07-17 08:37:06 +1000

    Contact Form using ARV, has inbuilt validation

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Col, Row, Button, Form, FormFeedback, FormGroup, Label, Input } from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
+// import { validateEmail } from '../../utils/helpers';
 
-const Example = (props) => {
+const Contact = (props) => {
     return (
-        <Form>
-            <Row form>
-                <Col md={6}>
-                    <FormGroup>
-                        <Label for="name">Name</Label>
-                        <Input type="text" name="name" id="name" placeholder="please enter your name" />
-                    </FormGroup>
-                </Col>
-                <Col md={6}>
-                    <FormGroup>
-                        <Label for="email">Email</Label>
-                        <Input type="email" name="email" id="email" placeholder="please enter your email address" />
-                    </FormGroup>
-                </Col>
-            </Row>
-            <FormGroup>
-                <Label for="message">Message</Label>
-                <Input type="text" name="message" id="message" placeholder="Please enter a message" />
-            </FormGroup>
-            <Button>Submit</Button>
-        </Form>
+        <AvForm>
+        <AvField name="name" label="Name" type="text" validate={{
+            required: {value: true, errorMessage: 'Please enter a name'},
+            pattern: {value: '^[A-Za-z0-9]+$', errorMessage: 'Your name must be composed only with letter and numbers'},
+            minLength: {value: 6, errorMessage: 'Your name must be between 6 and 16 characters'},
+            maxLength: {value: 16, errorMessage: 'Your name must be between 6 and 16 characters'}
+          }} />
+        <AvField name="email" label="Email" type="text" validate={{
+            required: {value: true, errorMessage: 'Please enter an email address'},
+            pattern: {value: '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$'}
+        }} />
+        <AvField name="message" label="Message" type="text" validate={{
+            required: {value: true, errorMessage: 'Please enter a message'},
+            pattern: {value: '^[A-Za-z0-9]+$', errorMessage: 'Your message must be composed only with letter and numbers'},
+            minLength: {value: 6, errorMessage: 'Your message must be at least 6 characters'},
+          }}/>
+        <Button color="primary">Submit</Button>
+      </AvForm>
     );
 }
 
-export default Example;
+export default Contact;
