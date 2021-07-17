decff24742c2de122c10528d8b37df0abb2820aa
Cat Ormerod <catormerod@gmail.com>
2021-07-17 11:00:19 +1000

    Added customHook for document title

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {useDocTitle} from '../../utils/customHooks.js'
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavLink, NavItem } from 'reactstrap';
 import './nav.css';
 import {
@@ -11,6 +12,7 @@ const Navigation = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   let location = useLocation();
+  const [doctitle, setDocTitle] = useDocTitle("CatOrmerod :: About");
 
   return (
     <div>
@@ -22,24 +24,28 @@ const Navigation = (props) => {
             <NavItem>
               <NavLink
                 className={location.pathname === '/about' ? 'active' : ''}
+                onClick={() => setDocTitle("CatOrmerod :: About")}
               ><Link to='/about'>About</Link>
               </NavLink>
             </NavItem>
             <NavItem>
               <NavLink
                 className={location.pathname === '/portfolio' ? 'active' : ''}
+                onClick={() => setDocTitle("CatOrmerod :: Portfolio")}
               ><Link to='/portfolio'>Portfolio</Link>
               </NavLink>
             </NavItem>
             <NavItem>
               <NavLink
                 className={location.pathname === '/resume' ? 'active' : ''}
+                onClick={() => setDocTitle("CatOrmerod :: Resume")}
               ><Link to='/resume'>Resume</Link>
               </NavLink>
             </NavItem>
             <NavItem>
               <NavLink
                 className={location.pathname === '/contact' ? 'active' : ''}
+                onClick={() => setDocTitle("CatOrmerod :: Contact")}
               ><Link to='/contact'>Contact</Link>
               </NavLink>
             </NavItem>
