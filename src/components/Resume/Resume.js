a8c474bceb9aabca297c329378f6c3eed8574f3d
Cat Ormerod <catormerod@gmail.com>
2021-07-21 13:01:04 +1000

    adjusted layout

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Jumbotron } from 'reactstrap';
+import { Container, Jumbotron, Row, Col } from 'reactstrap';
 import './resume.css';
 import myResume from '../../../src/assets/resume/resume.pdf';
 
@@ -10,18 +10,19 @@ export default function Resume() {
     <h3>Cat Ormerod - Resume</h3>
     <a id="resume-link" href={myResume} download="Cat-Ormerod-Resume">Download Resume</a>
     </Jumbotron>
-    <div className="container py-4">
-    <div className="row apgn-items-md-stretch">
-        <div className="col-md-12">
-            <div className="h-100 p-2 rounded-3">
+    <Row>
+        <Col>
+        <div className="h-100 p-2 rounded-3">
                 <p>Currently working towards a certificate in Full Stack Web Development from the University of Sydney
                     Coding Bootcamp.</p>
                 <p>Based in the Greater Sydney area, I'm looking for remote working opportunities</p>
                 
             </div>
-        </div>
-        <div className="col-md-4">
-            <div className="h-100 p-2 rounded-3">
+        </Col>
+    </Row>
+    <Row>
+        <Col className="col-md-4">
+        <div className="h-100 p-2 rounded-3">
                 <h4>Skills</h4>
                 <ul>
                 <h6>Browser Based Technologies</h6>
@@ -68,9 +69,10 @@ export default function Resume() {
                 <ul>Service Workers</ul>
                 </ul>
             </div>
-        </div>
-        <div className="col-md-8">
-            <div className="h-100 p-2 rounded-3">
+        </Col>
+        <Col className="col-md-8">
+        <Row>
+        <div className="h-100 p-2 rounded-3">
                 <h4>More about me</h4>
                 <p>I am a results-oriented project management specialist with a proven track record delivering
                     significant campaigns with challenging deadlines and strict budgets.</p>
@@ -79,17 +81,17 @@ export default function Resume() {
                 <p>Excellent communication and interpersonal skills, with the ability to develop solid strategic
                     relationships with customers, business partners and stakeholders. </p>
             </div>
-        </div>
-        <div className="col-md-8">
-            <div className="h-100 p-2 rounded-3">
+        </Row>
+        <Row>
+        <div className="h-100 p-2 rounded-3">
                 <h4>Career Highlights</h4>
                 <p>Seconded to the new office in Singapore to deliver events, marketing campaigns, promotions and set up processes and ongoing tools for the team.</p>
                 <p>Developing the Airpoints programme marketing launch for the new retail coalition – this involved working with 4 key business partners to deliver a priming and launch campaign that met objectives for all partners included.</p>
                 <p>Nominated finalist for the ICON award in December 2015 at Air NZ, as well as ‘Quest for the Best’ award at SKYCITY, both internal staff recognition programmes.</p>
             </div>
-        </div>
-    </div>
-    </div>
+        </Row>
+        </Col>
+    </Row>
     </Container>
   );
 }
