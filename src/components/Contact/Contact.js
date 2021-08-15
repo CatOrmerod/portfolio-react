b8bf8833b9b889f4bc6875997f07cdee434b2d82
Cat Ormerod <catormerod@gmail.com>
2021-08-16 09:48:57 +1000

    Changed contact form to trigger email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,49 +1,58 @@
-import React from 'react';
-import { Col, Row, Button, Jumbotron, Container } from 'reactstrap';
-import { AvForm, AvField } from 'availity-reactstrap-validation';
-// import { useForm, ValidationError } from '@formspree/react';
-// import { validateEmail } from '../../utils/helpers';
+import React, { useState } from 'react';
+import { Container, Jumbotron } from 'reactstrap';
+import { validateEmail } from '../../utils/helpers';
 
-const Contact = (props) => {
-  // const [state, handleSubmit] = useForm("meqvlrja");
-  // if (state.succeeded) {
-  //     return <p>Thanks for joining!</p>;
-  // }
-    return (
-      <Container>
-        <Jumbotron className="text-center">
-            <h3>Contact Me</h3>
-            <p className="lead">Please contact me at <a
-                        href="mailto:catormerod@gmail.com">catormerod@gmail.com</a> or use the form below!</p>
-        </Jumbotron>
-        <form action="https://formspree.io/f/meqvlrja" method="POST">
-        <AvForm>
-        <Row xs="1" sm="2">
-        <Col>
-        <AvField name="name" label="Name" type="text" validate={{
-            required: {value: true, errorMessage: 'Please enter a name'},
-            pattern: {value: '^[A-Za-z0-9]+$', errorMessage: 'Your name must be composed only with letter and numbers'},
-            minLength: {value: 2, errorMessage: 'Your name must be between 2 and 16 characters'},
-            maxLength: {value: 16, errorMessage: 'Your name must be between 2 and 16 characters'}
-          }} />
-        </Col>
-          <Col>
-          <AvField name="email" label="Email" type="text" validate={{
-            required: {value: true, errorMessage: 'Please enter an email address'},
-            email: {value: true, errorMessage: 'Please enter a valid email address'}
-        }} />
-        </Col>
-        </Row>
-        <AvField name="message" label="Message" type="text" validate={{
-            required: {value: true, errorMessage: 'Please enter a message'},
-            pattern: {value: '^[A-Za-z0-9]+$', errorMessage: 'Your message must be composed only with letter and numbers'},
-            minLength: {value: 6, errorMessage: 'Your message must be at least 6 characters'},
-          }}/>
-        <Button color="primary">Submit</Button>
-      </AvForm>
+function Contact() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+
+      if (!isValid) {
+        setErrorMessage('Please enter a valid email!');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`Your ${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  };
+
+  return (
+    <Container>
+      <Jumbotron className="text-center">
+        <h3>Contact Me</h3>
+        <p className="lead">Please contact me at <a
+          href="mailto:catormerod@gmail.com">catormerod@gmail.com</a> or use the form below!</p>
+      </Jumbotron>
+      <form action="https://formspree.io/f/meqvlrja" method="POST">
+        <label className="form-label" htmlFor="name">Name</label>
+        <input type="text" className="form-control" id="name" name="name" placeholder="Your name" onBlur={handleChange} />
+        <label className="form-label" htmlFor="email">Email</label>
+        <input type="text" className="form-control" id="email" name="email" placeholder="Your email" onBlur={handleChange} />
+        <label className="form-label" htmlFor="message">Message</label>
+        <textarea className="form-control" id="message" name="message" placeholder="Leave a message!" onBlur={handleChange} ></textarea>
+        {errorMessage && (
+          <div>
+            <p>{errorMessage}</p>
+          </div>
+        )}
+        {!errorMessage && (
+          <button className="btn" type="submit">SEND</button>
+        )}
       </form>
-      </Container>
-    );
+    </Container>
+  );
 }
 
 export default Contact;
